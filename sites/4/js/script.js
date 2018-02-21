eccd4de583393b590fec17b2f62dd9f478611415
Alexey Velichko <gamer315@yandex.ru>
2018-02-21 14:44:06 +0300

    1

---
+++ sites/4/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+      $('.slider').bxSlider({
+      	pager: false,
+      	
+      });
+    })
+
+$(document).ready(function(){
+      $('.banner').bxSlider({
+      	controls: false,
+      	auto: true,
+      	pause: 4000
+      	
+      });
+    })
