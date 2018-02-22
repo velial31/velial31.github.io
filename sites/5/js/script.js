1dbd2954bf6823e9bb785f02cf88be3fd9034217
Alexey Velichko <gamer315@yandex.ru>
2018-02-22 21:39:40 +0300

    commit

---
+++ sites/5/js/script.js
@@ -0,0 +1,16 @@
+// $(document).ready(function(){
+//       $('.slider').bxSlider({
+//       	pager: false,
+//       	auto: true,
+//       	pause: 4000
+//       });
+//     })
+
+$(document).ready(function(){
+  $('.slider').slick({
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 4000
+  });
+});
+
